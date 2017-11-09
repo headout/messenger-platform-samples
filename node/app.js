22dbca69e8695c7c8d729c0b0740c32c4b63e823
Kunal Khandelwal <kunal@headout.com>
2017-11-09 17:21:47 +0530

    added receipt text

--- node/app.js
+++ node/app.js
@@ -632,60 +632,42 @@ function sendReceiptMessage(recipientId) {
   // Generate a random receipt ID as the API requires a unique ID
   var receiptId = "order" + Math.floor(Math.random()*1000);
 
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message:{
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "receipt",
-          recipient_name: "Peter Chang",
-          order_number: receiptId,
-          currency: "USD",
-          payment_method: "Visa 1234",        
-          timestamp: "1428444852", 
-          elements: [{
-            title: "Oculus Rift",
-            subtitle: "Includes: headset, sensor, remote",
-            quantity: 1,
-            price: 599.00,
-            currency: "USD",
-            image_url: SERVER_URL + "/assets/riftsq.png"
-          }, {
-            title: "Samsung Gear VR",
-            subtitle: "Frost White",
-            quantity: 1,
-            price: 99.99,
-            currency: "USD",
-            image_url: SERVER_URL + "/assets/gearvrsq.png"
-          }],
-          address: {
-            street_1: "1 Hacker Way",
-            street_2: "",
-            city: "Menlo Park",
-            postal_code: "94025",
-            state: "CA",
-            country: "US"
-          },
-          summary: {
-            subtotal: 698.99,
-            shipping_cost: 20.00,
-            total_tax: 57.67,
-            total_cost: 626.66
-          },
-          adjustments: [{
-            name: "New Customer Discount",
-            amount: -50
-          }, {
-            name: "$100 Off Coupon",
-            amount: -100
-          }]
-        }
-      }
-    }
-  };
+	var messageData = {
+		recipient: {
+			id: recipientId
+		},
+	};
+
+	request(`https://www.test-headout.com/api/v1/order/secure?secureId=`, (error, response, body) => {
+		if (!error && response.statusCode === 200) {
+
+			const orderDetails = response.json();
+			const {card, productName, id, pricePayed, variantName, productId} = orderDetails;
+			console.log(orderDetails);
+
+			messageData.message = {
+				attachment: {
+					type: "template",
+					payload: {
+						template_type: "receipt",
+						order_number: id,
+						currency: pricePayed.code,
+						payment_method: `${card.brand} ${card.last4}`,
+						timestamp: `${orderDetails.time / 1000}`,
+						elements: [{
+							title: productName,
+							subtitle: variantName,
+							price: pricePayed,
+							currency: pricePayed.code,
+							image_url: `${UrlUtils.getApiBaseUrl()}/api/v3/tour-group/get/id/${productId}/image`,
+						}],
+					}
+				}
+			};
+		} else {
+			console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+		}
+	});
 
   callSendAPI(messageData);
 }
